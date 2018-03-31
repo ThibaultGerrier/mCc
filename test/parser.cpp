d8fddeff4ad4d88d6578e86b433d1c0af2d4e58d
Philipp-p <Philipp-p@users.noreply.github.com>
2018-03-31 16:50:10 +0200

    added test for unary minus, precedence and complete test for complex math

--- test/parser.cpp
+++ test/parser.cpp
@@ -73,8 +73,61 @@ TEST(Parser, NestedExpression_1)
 	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_LITERAL, subexpr->binary_op.rhs->type);
 
 	// subexpr -> rhs -> literal
-	ASSERT_EQ(MCC_AST_LITERAL_TYPE_FLOAT, subexpr->binary_op.rhs->literal->type);
+	ASSERT_EQ(MCC_AST_LITERAL_TYPE_FLOAT,
+	          subexpr->binary_op.rhs->literal->type);
 	ASSERT_EQ(3.14, subexpr->binary_op.rhs->literal->f_value);
+
+	mCc_ast_delete_expression(expr);
+}
+
+TEST(Parser, PrecedenceTest)
+{
+	const char input[] = "-42 * 192 + 3.14";
+	auto result = mCc_parser_parse_string(input);
+
+	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
+
+	auto expr = result.expression;
+
+	// root
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_BINARY_OP, expr->type);
+	ASSERT_EQ(MCC_AST_BINARY_OP_ADD, expr->binary_op.op);
+
+	// root -> lhs
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_BINARY_OP, expr->binary_op.lhs->type);
+	ASSERT_EQ(MCC_AST_BINARY_OP_MUL, expr->binary_op.lhs->binary_op.op);
+
+	// root -> lhs -> lhs
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_UNARY_OP,
+	          expr->binary_op.lhs->binary_op.lhs->type);
+	ASSERT_EQ(MCC_AST_UNARY_OP_MINUS,
+	          expr->binary_op.lhs->binary_op.lhs->unary_op.op);
+
+	// root -> lhs -> lhs -> rhs
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_LITERAL,
+	          expr->binary_op.lhs->binary_op.lhs->unary_op.rhs->type);
+
+	// root -> lhs -> lhs -> rhs ->literal
+	ASSERT_EQ(MCC_AST_LITERAL_TYPE_INT,
+	          expr->binary_op.lhs->binary_op.lhs->unary_op.rhs->literal->type);
+	ASSERT_EQ(
+	    42, expr->binary_op.lhs->binary_op.lhs->unary_op.rhs->literal->i_value);
+
+	// root -> lhs ->rhs
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_LITERAL,
+	          expr->binary_op.lhs->binary_op.rhs->type);
+
+	// root -> lhs -> rhs ->literal
+	ASSERT_EQ(MCC_AST_LITERAL_TYPE_INT,
+	          expr->binary_op.lhs->binary_op.rhs->literal->type);
+	ASSERT_EQ(192, expr->binary_op.lhs->binary_op.rhs->literal->i_value);
+
+	// root -> rhs
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_LITERAL, expr->binary_op.rhs->type);
+
+	// root -> rhs ->literal
+	ASSERT_EQ(MCC_AST_LITERAL_TYPE_FLOAT, expr->binary_op.rhs->literal->type);
+	ASSERT_EQ(3.14, expr->binary_op.rhs->literal->f_value);
 
 	mCc_ast_delete_expression(expr);
 }
@@ -97,13 +150,78 @@ TEST(Parser, WrongOperatorTest)
 
 TEST(Parser, ComplexMathExpressionTest)
 {
-	const char input[] = "((1 + 8) * !(8 == 8))";
+	const char input[] = "((1 + 8) / (8 == 8))";
 	auto result = mCc_parser_parse_string(input);
 
 	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
+
+	auto expr = result.expression;
+
+	// root -> paren
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_PARENTH, expr->type);
+
+	// root -> paren -> expr
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_BINARY_OP, expr->expression->type);
+	ASSERT_EQ(MCC_AST_BINARY_OP_DIV, expr->expression->binary_op.op);
+
+	// root -> paren -> expr -> lhs
+	auto sub_expr_lhs = expr->expression->binary_op.lhs;
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_PARENTH, sub_expr_lhs->type);
+
+	// root -> paren -> expr -> lhs -> paren -> expr
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_BINARY_OP,
+	          sub_expr_lhs->expression->type);
+	ASSERT_EQ(MCC_AST_BINARY_OP_ADD, sub_expr_lhs->expression->binary_op.op);
+
+	// root -> paren -> expr -> lhs -> paren -> expr -> lhs
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_LITERAL,
+	          sub_expr_lhs->expression->binary_op.lhs->type);
+
+	// root -> paren -> expr -> lhs -> paren -> expr -> lhs ->literal
+	ASSERT_EQ(MCC_AST_LITERAL_TYPE_INT,
+	          sub_expr_lhs->expression->binary_op.lhs->literal->type);
+	ASSERT_EQ(1, sub_expr_lhs->expression->binary_op.lhs->literal->i_value);
+
+	// root -> paren -> expr -> lhs -> paren -> expr -> rhs
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_LITERAL,
+	          sub_expr_lhs->expression->binary_op.rhs->type);
+
+	// root -> paren -> expr -> lhs -> paren -> expr -> rhs ->literal
+	ASSERT_EQ(MCC_AST_LITERAL_TYPE_INT,
+	          sub_expr_lhs->expression->binary_op.rhs->literal->type);
+	ASSERT_EQ(8, sub_expr_lhs->expression->binary_op.rhs->literal->i_value);
+
+	// root -> paren -> expr -> rhs
+	auto sub_expr_rhs = expr->expression->binary_op.rhs;
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_PARENTH, sub_expr_rhs->type);
+
+	// root -> paren -> expr -> rhs -> paren -> expr
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_BINARY_OP,
+	          sub_expr_rhs->expression->type);
+	ASSERT_EQ(MCC_AST_BINARY_OP_EQUALS, sub_expr_rhs->expression->binary_op.op);
+
+	// root -> paren -> expr -> rhs -> paren -> expr -> lhs
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_LITERAL,
+	          sub_expr_rhs->expression->binary_op.lhs->type);
+
+	// root -> paren -> expr -> rhs -> paren -> expr -> lhs ->literal
+	ASSERT_EQ(MCC_AST_LITERAL_TYPE_INT,
+	          sub_expr_rhs->expression->binary_op.lhs->literal->type);
+	ASSERT_EQ(8, sub_expr_rhs->expression->binary_op.lhs->literal->i_value);
+
+	// root -> paren -> expr -> rhs -> paren -> expr -> rhs
+	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_LITERAL,
+	          sub_expr_rhs->expression->binary_op.rhs->type);
+
+	// root -> paren -> expr -> rhs -> paren -> expr -> rhs ->literal
+	ASSERT_EQ(MCC_AST_LITERAL_TYPE_INT,
+	          sub_expr_rhs->expression->binary_op.rhs->literal->type);
+	ASSERT_EQ(8, sub_expr_rhs->expression->binary_op.rhs->literal->i_value);
+
+	mCc_ast_delete_expression(expr);
 }
 
-TEST(Parser, UnaryOperatorTest)
+TEST(Parser, UnaryOperatorExclamationTest)
 {
 	const char input[] = "!42";
 	auto result = mCc_parser_parse_string(input);
@@ -115,8 +233,29 @@ TEST(Parser, UnaryOperatorTest)
 	EXPECT_EQ(MCC_AST_EXPRESSION_TYPE_UNARY_OP, expr->type);
 	ASSERT_EQ(1, expr->node.sloc.start_col);
 
-    EXPECT_EQ(MCC_AST_EXPRESSION_TYPE_LITERAL, expr->unary_op.rhs->type);
-    ASSERT_EQ(2, expr->unary_op.rhs->node.sloc.start_col);
+	EXPECT_EQ(MCC_AST_EXPRESSION_TYPE_LITERAL, expr->unary_op.rhs->type);
+	ASSERT_EQ(2, expr->unary_op.rhs->node.sloc.start_col);
+
+	EXPECT_EQ(MCC_AST_EXPRESSION_TYPE_LITERAL, expr->unary_op.rhs->type);
+	ASSERT_EQ(2, expr->unary_op.rhs->literal->node.sloc.start_col);
+
+	mCc_ast_delete_expression(expr);
+}
+
+TEST(Parser, UnaryOperatorMinusTest)
+{
+	const char input[] = "-42";
+	auto result = mCc_parser_parse_string(input);
+
+	auto expr = result.expression;
+
+	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
+
+	EXPECT_EQ(MCC_AST_EXPRESSION_TYPE_UNARY_OP, expr->type);
+	ASSERT_EQ(1, expr->node.sloc.start_col);
+
+	EXPECT_EQ(MCC_AST_EXPRESSION_TYPE_LITERAL, expr->unary_op.rhs->type);
+	ASSERT_EQ(2, expr->unary_op.rhs->node.sloc.start_col);
 
 	EXPECT_EQ(MCC_AST_EXPRESSION_TYPE_LITERAL, expr->unary_op.rhs->type);
 	ASSERT_EQ(2, expr->unary_op.rhs->literal->node.sloc.start_col);
@@ -166,10 +305,12 @@ TEST(Parser, SourceLocation_SingleLineColumn)
 	EXPECT_EQ(MCC_AST_EXPRESSION_TYPE_BINARY_OP, expr->expression->type);
 	ASSERT_EQ(2, expr->expression->node.sloc.start_col);
 
-	EXPECT_EQ(MCC_AST_LITERAL_TYPE_INT, expr->expression->binary_op.lhs->literal->type);
+	EXPECT_EQ(MCC_AST_LITERAL_TYPE_INT,
+	          expr->expression->binary_op.lhs->literal->type);
 	ASSERT_EQ(2, expr->expression->binary_op.lhs->literal->node.sloc.start_col);
 
-	EXPECT_EQ(MCC_AST_LITERAL_TYPE_INT, expr->expression->binary_op.rhs->literal->type);
+	EXPECT_EQ(MCC_AST_LITERAL_TYPE_INT,
+	          expr->expression->binary_op.rhs->literal->type);
 	ASSERT_EQ(7, expr->expression->binary_op.rhs->literal->node.sloc.start_col);
 
 	mCc_ast_delete_expression(expr);
@@ -195,9 +336,12 @@ TEST(Parser, IdentifierTest)
 	ASSERT_EQ(1, id_expr->node.sloc.start_col);
 	ASSERT_EQ(11, id_expr->node.sloc.end_col);
 	ASSERT_EQ(0, strcmp("some_id123", id_expr->identifier->name));
-	ASSERT_EQ(0, strcmp("array", array_expr->array_identifier.identifier->name));
-	ASSERT_EQ(0, strcmp("id", array_expr->array_identifier.expression->array_identifier.identifier->name));
-	ASSERT_EQ(5, array_expr->array_identifier.expression->array_identifier.expression->literal->i_value);
+	ASSERT_EQ(0,
+	          strcmp("array", array_expr->array_identifier.identifier->name));
+	ASSERT_EQ(0, strcmp("id", array_expr->array_identifier.expression
+	                              ->array_identifier.identifier->name));
+	ASSERT_EQ(5, array_expr->array_identifier.expression->array_identifier
+	                 .expression->literal->i_value);
 
 	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_IDENTIFIER, id_expr->type);
 	ASSERT_EQ(MCC_AST_EXPRESSION_TYPE_ARRAY_IDENTIFIER, array_expr->type);
