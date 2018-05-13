130c779682445a5fedddd7854ed2b5fa5ea7d20e
Philipp-p <Philipp-p@users.noreply.github.com>
2018-05-13 02:25:02 +0200

    removed test that was not included in the language

--- test/type_checking.cpp
+++ test/type_checking.cpp
@@ -64,34 +64,6 @@ TEST(TypeChecking, SimpleArrayAssignmentCorrect)
 	mCc_err_delete_error_manager(error_manager);
 }
 
-TEST(TypeChecking, ComplexArrayAssignmentCorrect)
-{
-	const char input[] = "void main(){string[(3 + 15)] b; b[0] = \"Foo\";}";
-	auto result = mCc_parser_parse_string(input);
-
-	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
-
-	auto prog = result.program;
-
-	struct mCc_ast_symbol_table_visitor_data visitor_data = { nullptr, nullptr,
-		                                                      0 };
-
-	struct mCc_err_error_manager *error_manager = mCc_err_new_error_manager();
-	struct mCc_ast_function_def *cur_function;
-	auto symbol_table_visitor =
-	    mCc_ast_symbol_table_visitor(&visitor_data, error_manager);
-	mCc_ast_visit_program(prog, &symbol_table_visitor);
-	auto type_checking_visitor =
-	    mCc_ast_type_checking_visitor(&cur_function, error_manager);
-	mCc_ast_visit_program(prog, &type_checking_visitor);
-
-	ASSERT_EQ(0u, error_manager->used);
-
-	mCc_parser_delete_result(&result);
-	mCc_sym_table_delete_tree(visitor_data.symbol_table_tree);
-	mCc_err_delete_error_manager(error_manager);
-}
-
 TEST(TypeChecking, SimpleAdditionCorrect)
 {
 	const char input[] = "void main(){int a; a = 123; int b; b = 321; a + b;}";
