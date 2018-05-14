c18065b546a8ef87be0eef4e7bfb3eb195a9e7cf
Philipp-p <Philipp-p@users.noreply.github.com>
2018-05-14 15:12:16 +0200

    test for using a return vaule of a function in a function call was added

--- test/type_checking.cpp
+++ test/type_checking.cpp
@@ -272,6 +272,38 @@ TEST(TypeChecking, FunctionReturnAssigmentCorrect)
 	mCc_err_delete_error_manager(error_manager);
 }
 
+TEST(TypeChecking, FunctionChainReturnAssigmentCorrect)
+{
+	const char input[] = "float foo(){return 1.0;} int bar(float in){return "
+	                     "42;} void main(){int a; a = bar(foo());}";
+	auto result = mCc_parser_parse_string(input);
+
+	ASSERT_EQ(MCC_PARSER_STATUS_OK, result.status);
+
+	auto prog = result.program;
+
+	struct mCc_ast_symbol_table_visitor_data visitor_data = { nullptr, nullptr,
+		                                                      0 };
+
+	struct mCc_err_error_manager *error_manager = mCc_err_new_error_manager();
+	struct mCc_ast_function_def *cur_function;
+	auto symbol_table_visitor =
+	    mCc_ast_symbol_table_visitor(&visitor_data, error_manager);
+	mCc_ast_visit_program(prog, &symbol_table_visitor);
+	auto type_checking_visitor =
+	    mCc_ast_type_checking_visitor(&cur_function, error_manager);
+	mCc_ast_visit_program(prog, &type_checking_visitor);
+
+	const ::testing::TestInfo *const test_info =
+	    ::testing::UnitTest::GetInstance()->current_test_info();
+	print_all_errors(test_info->name(), error_manager);
+	ASSERT_EQ(0u, error_manager->used);
+
+	mCc_parser_delete_result(&result);
+	mCc_sym_table_delete_tree(visitor_data.symbol_table_tree);
+	mCc_err_delete_error_manager(error_manager);
+}
+
 TEST(TypeChecking, SimpleIfTypeCheckCorrectI)
 {
 	const char input[] = "void main(){bool a; a = true; if (a){return;}}";
